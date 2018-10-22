5cf47bef2cb8ec23565381691fda0ad0e98060d6
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 19:32:12 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(input)
-  if input
-    puts input
-    input
-  else
-    puts "Squawk!"
-    "Squawk!"
-  end
+def parrot(input = "Squawk!")
+  puts input
+  input
 end
